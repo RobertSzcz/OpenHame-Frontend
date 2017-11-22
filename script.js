bd4f11213b6a2c9e50d04df972a9c99a5f11d37a
RobertSzcz <robert.szczygiel22@gmail.com>
2017-11-22 17:32:26 +0200

    DS research

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+var api = 'https://jsonplaceholder.typicode.com';
+
+var DS = new Vue({
+  methods: {
+    getIndex: function(){
+      axios.get(api + '/posts/1')
+        .then(function (response) {
+          return response
+        })
+        .catch(function (error) {
+          return error
+        })
+    }
+  }
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    name: 'Vue.js',
+  },
+  // define methods under the `methods` object
+  methods: {
+    greet: function (event) {
+      //boundFn - should return object instead
+      var data = DS.getIndex
+      console.log(data)
+    }
   }
 })
