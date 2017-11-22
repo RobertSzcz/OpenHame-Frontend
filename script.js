49cdbc5f16c82013659e638cd382ce989260343b
RobertSzcz <robert.szczygiel22@gmail.com>
2017-11-23 01:27:27 +0200

    create working callback-driven DS

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 var api = 'https://jsonplaceholder.typicode.com';
 
-var DS = new Vue({
-  methods: {
-    getIndex: function(){
-      axios.get(api + '/posts/1')
-        .then(function (response) {
-          return response
-        })
-        .catch(function (error) {
-          return error
-        })
-    }
+var DS = {
+  getIndex: function(params, callback){
+    axios.get(api + params)
+      .then(function (response) {
+        callback(response.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+        callback(error)
+      })
   }
-})
+}
 
 var app = new Vue({
   el: '#app',
   data: {
     name: 'Vue.js',
+    somedata: ''
   },
-  // define methods under the `methods` object
   methods: {
     greet: function (event) {
-      //boundFn - should return object instead
-      var data = DS.getIndex
-      console.log(data)
+      vm = this
+      DS.getIndex('/posts/1', function(data){
+        vm.somedata = data
+      })
+      console.log(vm.somedata)
     }
   }
 })
