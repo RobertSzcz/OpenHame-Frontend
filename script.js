aefc0886fb1c7589c6dbdb68c7611f21ed07e507
tumbbi2 <tuomas.harju@student.hamk.fi>
2017-11-27 18:01:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const API = 'https://jsonplaceholder.typicode.com'
+const API = 'https://api.hel.fi/linkedevents/v1/'
+const API_EVENT = API + 'event'
+const API_PLACE = API + 'place'
 const API_KEY = 'API_KEY123'
 
 var DS = {
-  getIndex: function(params, callback) {
-    var request_params = {}
-    _.merge(request_params, {
-      api_key: API_KEY
-    }, params)
+  getEvents: function(params, callback) {
+	axios({
+        method: 'get',
+        url: API_EVENT,
+        params: params
+      })
+      .then(function(response) {
+        callback(response.data)
+      })
+      .catch(function(error) {
+        console.log(error)
+        callback(error)
+      })
+  },
+
+  getPlaces: function(callback) {
     axios({
         method: 'get',
-        url: API + '/posts/1',
-        params: request_params
+        url: API_PLACE,
       })
       .then(function(response) {
         callback(response.data)
@@ -25,18 +37,23 @@ var DS = {
 var app = new Vue({
   el: '#app',
   data: {
-    name: 'Vue.js',
-    somedata: ''
+    division: '',
+    text: '',
+    start: '',
+    end: '',
+    events: ''
   },
   methods: {
     greet: function(event) {
       var vm = this
       vm.somedata = 'loading...'
-      DS.getIndex({
-        test_param: 1
-      }, function(data) {
-        vm.somedata = data
+      DS.getEvents({division:vm.division, text:vm.text, start:vm.start, end:vm.end}, function(data) {
+        vm.events = data.data
       })
+	  
+	  
+	  
+	  
     }
   }
 })
